2d884a925a47d1241bc3aefb02b396a67af26f1e
Robert Sander <gurubert@gurubert.de>
2013-10-19 23:51:58 +0200

    Prefix reply with content of $RBLMESSAGE Replace %IP% with $TCPREMOTEIP

--- net/ucspi-tcp6-0.99a/src/rblsmtpd.c
+++ net/ucspi-tcp6-0.99a/src/rblsmtpd.c
@@ -216,12 +216,37 @@ struct commands smtpcommands[] = {
 void rblsmtpd(void)
 {
   int i;
+  char *rblmessage;
+
+  rblmessage = env_get("RBLMESSAGE");
 
   if (flagmustnotbounce || (decision == 2) || (decision == 4)) {
     if (!stralloc_copys(&message,"451 ")) nomem();
   }
   else
     if (!stralloc_copys(&message,"553 ")) nomem();
+
+  if (rblmessage)
+    if (!stralloc_cats(&message, rblmessage))
+      nomem();
+
+  /* replace %IP% with content of $TCPREMOTEIP in RBLSMTPD string */
+  for (i = 0;i <= ( text.len - 4 );++i)
+    if (text.s[i] == '%' && text.s[i+1] == 'I' && text.s[i+2] == 'P' && text.s[i+3] == '%') {
+
+      if (!stralloc_copyb(&tmp,text.s,i)) nomem();
+
+      if (!stralloc_cats(&tmp,ip_env)) nomem();
+
+      if (i < ( text.len - 4)) {
+	if (!stralloc_catb(&tmp,text.s + i + 4, text.len - i - 4)) nomem();
+      }
+
+      if (!stralloc_copy(&text,&tmp)) nomem();
+
+      i = i + 4;
+    }
+
 
   if (text.len > 200) text.len = 200;
   if (!stralloc_cat(&message,&text)) nomem();
